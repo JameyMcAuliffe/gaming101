0cbea490770950de2c71abcf2b7936d2921e36b8
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2016-10-05 09:35:27 -0500

    board originally drwn from font end only

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => res.render('index'))
 
+
+mongoose.Promise = Promise
 mongoose.connect(MONGODB_URL, () => {
 	server.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
 })
@@ -31,7 +33,25 @@ const Game = mongoose.model('game', {
 })
 
 io.on('connect', socket => {
+	Game.create({
+		board:[['','',''],['','',''],['','','']]
+	})
+	.then(g => {
+		socket.game = g
+		socket.emit('new game', g)
+	})
+	.catch(err => {
+		socket.emit('error', err)
+		console.error(err)
+	})
+
+
 	console.log(`Socket connected: ${socket.id}`)
 	socket.on('disconnect', () => console.log('Socket disconnected'))
 })
+
+
+
+
+
 
